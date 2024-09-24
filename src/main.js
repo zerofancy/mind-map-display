916ecf05f0ab0c405f4b752b0df53adff697cec9
zerofancy <ntutn.top@gmail.com>
2024-09-25 02:56:00 +0800

    支持带markdown的备注显示

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import MindMap from "simple-mind-map";
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
+import { marked } from 'marked';
 
+const containerElement = document.getElementById('mindMapContainer')
 
-const mindMap = new MindMap({
-  el: document.getElementById('mindMapContainer'),
+let toastShowing = null
+
+new MindMap({
+  el: containerElement,
   data: {
     "data": {
         "text": "根节点"
@@ -17,5 +23,28 @@ const mindMap = new MindMap({
         },
       }
     ]
+  },
+  customNoteContentShow: {
+    show: (content, left, top) => {
+      if (toastShowing != null) {
+        return
+      }
+      toastShowing = Toastify({
+        text: marked.parse("**备注**\n\n" + content),
+        duration: 3000,
+        position: "left",
+        callback: () => {
+          toastShowing = null
+        },
+        offset: {
+          x: left,
+          y: top
+        },
+        escapeMarkup: false
+      })
+      toastShowing.showToast();
+    },
+    hide: () => {
+    }
   }
 });
