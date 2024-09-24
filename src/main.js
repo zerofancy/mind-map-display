57d4315fc6e3f1905c09a48b280a8ae4b9c9a010
zerofancy <ntutn.top@gmail.com>
2024-09-25 03:41:38 +0800

    基本编译

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import MindMap from "simple-mind-map";
 import Toastify from 'toastify-js'
 import "toastify-js/src/toastify.css"
 import { marked } from 'marked';
+import { metaData } from "../meta-generated.js"
 
 const containerElement = document.getElementById('mindMapContainer')
 
@@ -9,21 +10,7 @@ let toastShowing = null
 
 new MindMap({
   el: containerElement,
-  data: {
-    "data": {
-        "text": "根节点"
-    },
-    "children": [
-      {
-        "data": {
-          "text": "子节点1",
-          "hyperlink": "https://ntutn.top",
-          "hyperlinkTitle": "我的博客",
-          "note": "这是一个**测试**" // 备注是否支持markdown
-        },
-      }
-    ]
-  },
+  data: metaData,
   customNoteContentShow: {
     show: (content, left, top) => {
       if (toastShowing != null) {
